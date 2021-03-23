cef9ccd63e52c12c31267b8c7a988c09d13e2401
dennissimmo <denchik16062001@gmail.com>
2021-03-23 16:52:41 +0200

    Initial commit

---
+++ src/main/java/com/denchik/demo/FinanceBotApplication.java
@@ -0,0 +1,13 @@
+package com.denchik.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceBotApplication.class, args);
+    }
+
+}
